c37144094064303ca858743084946fd0cdd27070
Javier Sopesens <javisopesens@gmail.com>
2022-06-19 11:28:10 +0200

    trimWordsList tests

--- main/generate_trim_dict.py
+++ main/generate_trim_dict.py
@@ -1,4 +1,9 @@
 def getAllWords(file: str = 'es.txt') -> list: 
+    # input is str
+    if type(file) != str:
+        raise TypeError('file have to be string format')
+
+    # file exists
     from pathlib import Path
     my_file = Path('Dictionary\\'+file)
     if not my_file.exists():
@@ -9,15 +14,23 @@ def getAllWords(file: str = 'es.txt') -> list:
     return words
 
 
-def trimWordsList(words: list, wordSize: int = 5) -> list:
-    trimedList = [word+'\n' for word in words if len(word) == wordSize]
+def trimWordsList(words: list[str], wordSize: int = 5) -> list:
+    # if not all([words for word in words if type(word) == str]):
+    if not isinstance(words,list):
+        raise TypeError('format error: words')
+    if not type(wordSize) == int:
+        raise TypeError('format error: wordSize')
+    if wordSize < 0:
+        raise ValueError('word size cannot be negative')
+        
+    trimedList = [word for word in words if len(word) == wordSize]
     return trimedList
 
 
 def writeTrimedList(words: list):
     f = open("Dictionary/trimedDictionary.txt", "w+")
     for word in words:
-        f.write(word)
+        f.write(word+'\n')
 
 
 fullWordList = getAllWords()
