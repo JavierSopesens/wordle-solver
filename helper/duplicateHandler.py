d24de9d02e281740440351fdbc507bed9227f436
Javier Sopesens <javisopesens@gmail.com>
2023-05-14 20:56:17 +0200

    fix global call

--- helper/duplicateHandler.py
+++ helper/duplicateHandler.py
@@ -12,11 +12,11 @@ def controlRepeatedMatches(sorted_matches:list[Match], original_matches:list[Mat
     previous = sorted_matches[0]
     for current in sorted_matches[1:]:
         if current.letter == previous.letter:
-            if current.status is MatchStatus.NOT_IN_THE_WORD and previous.status is MatchStatus.NOT_IN_THE_WORD:
+            if current.status is MatchStatus.NOT_IN_WORD and previous.status is MatchStatus.NOT_IN_WORD:
                 original_matches[original_matches.index(previous)].status = MatchStatus.TO_AVOID
-            if current.status is MatchStatus.NOT_IN_THE_WORD and previous.status is not MatchStatus.NOT_IN_THE_WORD:
+            if current.status is MatchStatus.NOT_IN_WORD and previous.status is not MatchStatus.NOT_IN_WORD:
                 original_matches[original_matches.index(current)].status = MatchStatus.TO_AVOID
-            if current.status is not MatchStatus.NOT_IN_THE_WORD and previous.status is MatchStatus.NOT_IN_THE_WORD:
+            if current.status is not MatchStatus.NOT_IN_WORD and previous.status is MatchStatus.NOT_IN_WORD:
                 original_matches[original_matches.index(previous)].status = MatchStatus.TO_AVOID
         previous = current
     return original_matches
