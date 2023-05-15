3d897f1957b7cf63fd1040a92ef4358ba2c4580d
Javier Sopesens <javisopesens@gmail.com>
2023-05-15 22:40:37 +0200

    test file class

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,10 +1,23 @@
 import pytest
 from classes import File
+from config import FILE_PATH
 
 def test_no_args():
     with pytest.raises(TypeError):
         File()
 
-def test_invalid_name():
+def test_file_invalid_name():
     with pytest.raises(ValueError):
         File('invalid_path/invalid_file.txt')
+
+
+def test_getContentByLength_wrong_type():
+    myFile = File(FILE_PATH)
+    with pytest.raises(ValueError):
+        myFile.getContentByLength('a')
+
+def test_getContentByLength_right_behavior():
+    myFile = File('tests/mock_glossary.txt')
+    assert myFile.getContentByLength(1) == []
+    assert myFile.getContentByLength(4) == ['abba']
+    assert myFile.getContentByLength(7) == ['abdomen','abandon']
