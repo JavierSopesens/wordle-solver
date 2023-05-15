10849b10e8f677dcd783c1eefb1a38c3d5427fd9
Javier Sopesens <javisopesens@gmail.com>
2023-05-15 22:56:10 +0200

    update test_word class

--- classes/Word.py
+++ classes/Word.py
@@ -1,16 +1,18 @@
 class Word:
-    def __init__(self, letters:list[str]) -> None:
+    def __init__(self, letters: list[str]) -> None:
         if not (letters):
             raise ValueError('letters are necesary')
         self.letters = letters
 
-    def containsLetter(self, letter:str) -> bool:
+    def containsLetter(self, letter: str) -> bool:
         if len(letter) != 1:
             raise ValueError('you only can pass one letter')
         return letter in self.letters
-    
-    def hasLetterInPlace(self, letter:str, place:int)->bool:
+
+    def hasLetterInPlace(self, letter: str, place: int) -> bool:
+        if place < 0 or place > len(self.letters):
+            raise IndexError('place cannot be higher than number of letters in word')
         return letter in self.letters[place]
-    
-    def hasRepeatedLetters(self)->bool:
+
+    def hasRepeatedLetters(self) -> bool:
         return len(set(self.letters)) < len(self.letters)
